f5ee84dad700c30fdec8e266e1f66cb538cb2e5c
Agnes Ahlman <ahlman.agnes@gmail.com>
2023-03-08 15:02:30 +0100

    Setup prettier and eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:react/recommended',
+    'standard-with-typescript',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json'],
+  },
+  plugins: ['react', 'react-hooks', '@typescript-eslint', 'prettier'],
+  rules: {},
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+}
