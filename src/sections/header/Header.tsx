01cf8151b191cd9746a25a134c981b0569c4ccd3
Agnes Ahlman <ahlman.agnes@gmail.com>
2023-04-28 13:13:12 +0200

    updated cv again

--- src/sections/header/Header.tsx
+++ src/sections/header/Header.tsx
@@ -21,7 +21,7 @@ const Header: React.FC = () => {
             <p className={styles.portfolio}>PORTFOLIO: AGNES AHLMAN </p>
 
             <div className={styles.cvLinkContainer}>
-              <a href="cv-AgnesAhlman.pdf" download="cv-AgnesAhlman.pdf" className={styles.cvLink}>
+              <a href="cv_agnesahlman.pdf" download="cv_agnesahlman.pdf" className={styles.cvLink}>
                 Download CV <IoMdArrowDropright />
               </a>
             </div>
